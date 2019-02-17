1ce073654fc68b99542eae92ed2cddac63794615
Kamal Kaur <kkaur337@my.centennialcollege.ca>
2019-02-16 23:34:50 -0500

    final commit

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,31 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'About Me' });
 });
 
-/* GET products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+/* GET project page. */
+router.get('/project', function(req, res, next) {
+  res.render('index', { title: 'Project Page' });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Service Page' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('index', { title: 'Contact Me' });
+});
+
+router.get('/projectList', function(req, res, next) {
+  res.render('index', { title: 'My Project List' });
 });
 
 module.exports = router;
